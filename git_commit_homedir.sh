41359c5dc406b0843ecbc733ed604ab88e65e526
jessebobrien <jesse.b.obrien@gmail.com>
2012-12-28 11:11:22 -0500

    reorganized

---
+++ git_commit_homedir.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~
+git add .
+git commit -m 'automated backup' .
---
+++ spy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create a list of users and direct it to a file.
+cat /etc/passwd |grep bash |grep home | awk -F: '{print $1}' > names.txt
+
+# Create an HTML file. This part must be outside the loop. This is a clunky implementation ... replace with if -d file.
+touch user_space.html
+mv user_space.html user_space.html.old
+touch user_space.html
+# Start the WHILE loop
+while read NAME
+	do
+		# This sets output of the command to the variable name, SPACE.
+		SPACE=`du -shm /home/$NAME | awk -F" " '{print $1}'`
+		echo " <br>" >> user_space.html
+	done < names.txt
+# End the table entry outside the loop.
+echo "
+User	|	Space<br>
+$NAME	|	$SPACE
+" >> user_space.html
